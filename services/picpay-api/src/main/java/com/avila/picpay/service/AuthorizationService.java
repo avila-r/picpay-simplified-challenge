97de881f39b3e6be382b9701730f711baab8ac25
avila <avila.dev@outlook.com>
2024-04-22 15:44:04 -0300

    feat: add notification main service

--- services/picpay-api/src/main/java/com/avila/picpay/service/AuthorizationService.java
+++ services/picpay-api/src/main/java/com/avila/picpay/service/AuthorizationService.java
@@ -1,6 +1,8 @@
 package com.avila.picpay.service;
 import com.avila.picpay.model.Authorization;
 import com.avila.picpay.model.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -8,7 +10,7 @@ import java.util.Objects;
 
 @Service
 public class AuthorizationService {
-
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationService.class);
     private final RestClient client;
 
     public AuthorizationService(RestClient.Builder client) {
@@ -18,6 +20,7 @@ public class AuthorizationService {
     }
 
     public void authorize(Transaction transaction){
+        logger.info("Authorizing transaction: {}", transaction);
         ResponseEntity<Authorization> authorization = client.get()
             .retrieve().toEntity(Authorization.class);
         if ((authorization.getStatusCode().isError()) && !(Objects.requireNonNull(authorization.getBody()).isAuthorized())) {
---
+++ services/picpay-api/src/main/java/com/avila/picpay/service/NotificationService.java
@@ -0,0 +1,36 @@
+package com.avila.picpay.service;
+import com.avila.picpay.exception.KafkaCommunicationException;
+import com.avila.picpay.model.Transaction;
+import com.avila.picpay.notification.KafkaProducer;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service @AllArgsConstructor
+public class NotificationService {
+    private final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+    private final KafkaProducer kafka;
+
+    public void notifyTransaction(Transaction transaction){
+        logger.info("Sending transaction notification to Kafka: {}", setTransactionNotificationTemplate(transaction));
+        try {
+            kafka.sendTransaction(transaction);
+        } catch (Exception e) {
+            logger.error("Error sending transaction notification to Kafka: {}", e.getMessage());
+            throw new KafkaCommunicationException("Error sending transaction notification to Kafka");
+        }
+    }
+
+    private @NotNull String setTransactionNotificationTemplate(@NotNull Transaction transaction){
+        return "Transaction" +
+                "{" +
+                "id: " + transaction.id() + ", " +
+                "sender: " + transaction.payer() + ", " +
+                "receiver: " + transaction.payee() + ", " +
+                "value: " + transaction.value() + ", " +
+                "timestamp: " + transaction.createdAt() + ", " +
+                "}";
+    }
+}
