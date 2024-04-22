dcedbfe8192f85d9bca3925477243288b50113b7
avila <avila.dev@outlook.com>
2024-04-22 14:54:35 -0300

    feat: add authorization service

---
+++ services/picpay-api/src/main/java/com/avila/picpay/service/AuthorizationService.java
@@ -0,0 +1,27 @@
+package com.avila.picpay.service;
+import com.avila.picpay.model.Authorization;
+import com.avila.picpay.model.Transaction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import java.util.Objects;
+
+@Service
+public class AuthorizationService {
+
+    private final RestClient client;
+
+    public AuthorizationService(RestClient.Builder client) {
+        this.client = client
+            .baseUrl("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc")
+            .build();
+    }
+
+    public void authorize(Transaction transaction){
+        ResponseEntity<Authorization> authorization = client.get()
+            .retrieve().toEntity(Authorization.class);
+        if ((authorization.getStatusCode().isError()) && !(Objects.requireNonNull(authorization.getBody()).isAuthorized())) {
+            transaction.refuse();
+        }
+    }
+}
