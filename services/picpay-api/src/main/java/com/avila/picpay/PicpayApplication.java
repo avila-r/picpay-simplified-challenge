42a7eeecded48157bc0a4df4f776bc28e80baf80
avila <avila.dev@outlook.com>
2024-04-23 15:18:34 -0300

    fix: change jpa to jdbc

--- services/picpay-api/src/main/java/com/avila/picpay/PicpayApplication.java
+++ services/picpay-api/src/main/java/com/avila/picpay/PicpayApplication.java
@@ -1,10 +1,10 @@
 package com.avila.picpay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJdbcAuditing
 public class PicpayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PicpayApplication.class, args);
