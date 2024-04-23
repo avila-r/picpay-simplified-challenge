c60e0e7468bb4ebab25e8ebcc701a5f6dec5ea6c
avila <avila.dev@outlook.com>
2024-04-23 16:00:32 -0300

    fix: request's body to param

--- services/picpay-api/src/main/java/com/avila/picpay/controller/TransactionController.java
+++ services/picpay-api/src/main/java/com/avila/picpay/controller/TransactionController.java
@@ -3,10 +3,8 @@ import com.avila.picpay.model.Transaction;
 import com.avila.picpay.repository.TransactionRepository;
 import com.avila.picpay.service.TransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController @AllArgsConstructor
@@ -16,7 +14,7 @@ public class TransactionController {
     private final TransactionService service;
 
     @PostMapping
-    public Transaction createTransaction(Transaction transaction){
+    public Transaction createTransaction(@RequestBody Transaction transaction){
         return service.create(transaction);
     }
 
