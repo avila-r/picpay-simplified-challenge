725e0c9c49b6ab5e82e34ffe315a3fc635d73725
avila <avila.dev@outlook.com>
2024-04-25 16:10:02 -0300

    fix: exception-secure insert endpoint | feat: /{id} get endpoint

--- services/picpay-api/src/main/java/com/avila/picpay/controller/CustomerController.java
+++ services/picpay-api/src/main/java/com/avila/picpay/controller/CustomerController.java
@@ -1,12 +1,11 @@
 package com.avila.picpay.controller;
-import com.avila.picpay.exception.InvalidCustomerTypeException;
+import com.avila.picpay.exception.CustomerNotFoundException;
+import com.avila.picpay.exception.InvalidCustomerRegistryException;
 import com.avila.picpay.model.Customer;
 import com.avila.picpay.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController @AllArgsConstructor
 @RequestMapping("/customer")
@@ -15,9 +14,29 @@ public class CustomerController {
 
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer){
-        if (customer.isCommonCustomer() || customer.isSellerCustomer()){
-            return repository.save(customer);
+        if (!customer.isCommonCustomer() && !customer.isSellerCustomer()){
+            throw new InvalidCustomerRegistryException("Invalid customer type");
         }
-        throw new InvalidCustomerTypeException("Invalid customer type");
+
+        if (repository.existsByEmail(customer.email())){
+            throw new InvalidCustomerRegistryException("Email already in use");
+        }
+
+        if (repository.existsByCpf(customer.cpf())){
+            throw new InvalidCustomerRegistryException("CPF already in use");
+        }
+
+        return repository.save(customer);
+    }
+
+    @GetMapping
+    public List<Customer> listAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer findById(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer not found"));
     }
 }
