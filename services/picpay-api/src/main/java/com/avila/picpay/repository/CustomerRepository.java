22a71671222d0a76e307ae9fb305a7a9764ac14e
avila <avila.dev@outlook.com>
2024-04-25 16:10:21 -0300

    feat: add repository special query methods

--- services/picpay-api/src/main/java/com/avila/picpay/repository/CustomerRepository.java
+++ services/picpay-api/src/main/java/com/avila/picpay/repository/CustomerRepository.java
@@ -4,4 +4,9 @@ import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends ListCrudRepository<Customer, Long> { }
+public interface CustomerRepository extends ListCrudRepository<Customer, Long> {
+    Customer findByCpf(Long cpf);
+    Customer findByEmail(String email);
+    boolean existsByCpf(Long cpf);
+    boolean existsByEmail(String email);
+}
